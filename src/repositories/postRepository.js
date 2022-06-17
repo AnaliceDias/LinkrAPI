01175d4411a4b8e70a0f92aaeb263cf0cf27ffe8
Analice <anadias.alice@gmail.com>
2022-06-16 23:47:59 -0300

    merge

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -15,7 +15,7 @@ async function getTimeline() {
     `SELECT p.id AS id, p.text AS text, p.link AS link, u.name AS name 
     FROM posts p
     JOIN users u
-    ON u.id=p."usersId"
+    ON u.id=p."userId"
     ORDER BY id DESC`)
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,13 @@
 import { Router } from "express";
 
 import authRouter from "./authRouter.js";
-<<<<<<< HEAD
-import hashtagsRouter from "./hashtagsRouter.js";
-=======
 import postRouter from "./postRouter.js";
->>>>>>> f89a20b78c5047cae108e00ce243690ff66107db
+import hashtagsRouter from "./hashtagsRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
-<<<<<<< HEAD
-router.use(hashtagsRouter)
-
-=======
 router.use(postRouter);
+router.use(hashtagsRouter);
 
-
->>>>>>> f89a20b78c5047cae108e00ce243690ff66107db
 export default router;
