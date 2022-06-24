13ea6ebce90e4959ffb3c592ac0a1c5a9571afba
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-06-23 22:31:44 -0300

    changed to ILIKE the search user request

--- src/controllers/headerController.js
+++ src/controllers/headerController.js
@@ -1,19 +1,13 @@
-import userRepository from "../repositories/userRepository.js"
+import userRepository from "../repositories/userRepository.js";
 
-export async function getUser(req,res){
-    const {character} = req.params
-    
-    try{
-        const user = await userRepository.getUserByCharacter(character)
-        
-        if(user.rows[0] === undefined){
-            res.status(400).send("User not found")
-        }
+export async function getUser(req, res) {
+  const { character } = req.params;
 
-        res.status(200).send(user.rows)
-        
-    } catch(error){
-        console.log(error)
-        res.status(500).send(error.detail)
-    }
+  try {
+    const user = await userRepository.getUserByCharacter(character);
+    res.status(200).send(user.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.detail);
+  }
 }
